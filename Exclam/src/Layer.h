408eedd0aa2f321f4f0bd09a95190884e6a6d8cb
ryu <wocks1123@gmail.com>
2020-01-31 23:12:29 +0900

    Layer 클래스 생성
    - 기본 레이어 구성을 위한 클래스
    - 이 클래스 상속을 통해 다른 레이어 구현 예정

---
+++ Exclam/src/Layer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "common.h"
+#include "Blob.h"
+
+#ifndef _JCEXCLAM_LAYER_H_
+#define _JCEXCLAM_LAYER_H_
+
+NAME_SPACE_S
+
+template <typename T>
+class Layer
+{
+protected:
+	size_t in;
+	size_t out;
+
+public:
+	Layer(size_t in, size_t out) : in(in), out(out)
+	{
+		printf("Layer Constructor called\n");
+
+	}
+
+	~Layer()
+	{
+		printf("Layer Destructor called\n");
+	}
+
+	virtual Blob<T> foward(const Blob<T>& x) = 0;
+	virtual Blob<T> backward(const Blob<T>& dL) = 0;
+};
+
+NAME_SPACE_E
+
+#endif
