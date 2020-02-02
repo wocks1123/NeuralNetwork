f62be18956124dfcf3ed9c0f99e6666b39761bf1
ryu <wocks1123@gmail.com>
2020-02-02 12:20:30 +0900

    행렬 연산 추가
    - 2차원 덧셈, 뺄셈, 스칼라곱 추가

--- Exclam/src/util.h
+++ Exclam/src/util.h
@@ -7,6 +7,42 @@
 
 NAME_SPACE_S
 
+void MatAdd(const float* A, const size_t a_h, const size_t a_w, const float* B, const size_t b_h, const size_t b_w, float* C)
+{
+	if (a_h == b_h && a_w == b_w)
+	{
+		fprintf(stderr, "The matrixes size is not matched...\n");
+		fprintf(stderr, "%s at %s::%05d\n", __func__, __FILE__, __LINE__);
+		abort();
+	}
+
+	for (int i = 0; i < a_h; i++)
+	{
+		for (int j = 0; j < b_w; j++)
+		{
+			ARR_I_J(C, i, b_w, j) = ARR_I_J(A, i, a_h, j) + ARR_I_J(B, i, b_h, j);
+		}
+	}
+}
+
+void MatSub(const float* A, const size_t a_h, const size_t a_w, const float* B, const size_t b_h, const size_t b_w, float* C)
+{
+	if (a_h == b_h && a_w == b_w)
+	{
+		fprintf(stderr, "The matrixes size is not matched...\n");
+		fprintf(stderr, "%s at %s::%05d\n", __func__, __FILE__, __LINE__);
+		abort();
+	}
+
+	for (int i = 0; i < a_h; i++)
+	{
+		for (int j = 0; j < b_w; j++)
+		{
+			ARR_I_J(C, i, b_w, j) = ARR_I_J(A, i, a_h, j) - ARR_I_J(B, i, b_h, j);
+		}
+	}
+}
+
 void MatMul(const float* A, const size_t a_h, const size_t a_w, const float* B, const size_t b_h, const size_t b_w, float* C)
 {
 	for (int i = 0; i < a_h; i++)
@@ -22,6 +58,18 @@ void MatMul(const float* A, const size_t a_h, const size_t a_w, const float* B, 
 		}
 	}
 }
+
+void MatMulValue(const float* A, const size_t a_h, const size_t a_w, float* C, float val)
+{
+	for (int i = 0; i < a_h; i++)
+	{
+		for (int j = 0; j < a_w; j++)
+		{
+			ARR_I_J(C, i, a_w, j) = ARR_I_J(A, i, a_w, j) * val;
+		}
+	}
+}
+
 
 void MatTranpose(const float* A, const size_t a_h, const size_t a_w, float* C)
 {
